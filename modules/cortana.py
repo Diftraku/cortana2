eae5d16caf1700d7ad0809f04b202e4633432f9f
Toni Kaija <toni.kaija@corefactory.com>
2019-10-23 00:27:33 +0300

    Fix the topic update

--- modules/cortana.py
+++ modules/cortana.py
@@ -10,15 +10,18 @@ from sopel.tools import SopelMemory
 
 STATUS_PREFIX = 'JMT11CD: '
 TOPIC_SEPARATOR = '|'
+BOT_NICK = 'Cortana'
+
 
 def setup(bot):
     if 'clubroom_status' not in bot.memory:
         bot.memory['clubroom_status'] = SopelMemory()
 
-@module.commands('open', 'closed')
-@module.rate(channel=5)
-def toggle_status(bot, trigger):
-    '''Toggle the clubroom status from open to closed and back'''
+
+@module.nickname_commands('open', 'auki', 'closed', 'kiinni')
+@module.rule(r'^Hey,?\s'+BOT_NICK+r',?\s?(.*)$')
+def set_clubroom_status(bot, trigger):
+    '''Set clubroom status'''
     bot.memory['clubroom_status'][trigger.sender] = trigger.group(1)
     # Update the channel topic, retaining the previous extra info
     update_status(bot, trigger.sender)
@@ -27,7 +30,7 @@ def update_status(bot, channel):
     '''Helper for updating the status from memory'''
     # Get the current topic and split it by the separator
     topic = bot.channels[channel].topic.split(TOPIC_SEPARATOR)
-    if topic.find(TOPIC_SEPARATOR) == -1:
+    if len(topic) == 1:
         # Pad the topic with a place for our status
         topic.insert(0, '')
     topic[0] = STATUS_PREFIX + \
