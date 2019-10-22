a864b0a81bdd8b05c5ec6cc16bc535b7983c369d
Toni Kaija <toni.kaija@corefactory.com>
2019-10-23 00:02:14 +0300

    Rework the status to support multiple channels

--- modules/cortana.py
+++ modules/cortana.py
@@ -5,35 +5,36 @@ Dedicated bot for the OtaHOAS clubroom at JMT11CD
 """
 from pathlib import Path
 from sopel import module
+from sopel.tools import SopelMemory
 
 
-CHANNEL = '#polygame'
 STATUS_PREFIX = 'JMT11CD: '
 TOPIC_SEPARATOR = '|'
 
 def setup(bot):
     if 'clubroom_status' not in bot.memory:
-        bot.memory['clubroom_status'] = 'unknown'
+        bot.memory['clubroom_status'] = SopelMemory()
 
 @module.commands('open', 'closed')
 @module.rate(channel=5)
 def toggle_status(bot, trigger):
     '''Toggle the clubroom status from open to closed and back'''
-    bot.memory['clubroom_status'] = trigger.group(1)
+    bot.memory['clubroom_status'][trigger.sender] = trigger.group(1)
     # Update the channel topic, retaining the previous extra info
-    update_status(bot)
+    update_status(bot, trigger.sender)
 
-def update_status(bot):
+def update_status(bot, channel):
     '''Helper for updating the status from memory'''
     # Get the current topic and split it by the separator
-    topic = bot.channels[CHANNEL].topic.split(TOPIC_SEPARATOR)
+    topic = bot.channels[channel].topic.split(TOPIC_SEPARATOR)
     if topic.find(TOPIC_SEPARATOR) == -1:
         # Pad the topic with a place for our status
         topic.insert(0, '')
-    topic[0] = STATUS_PREFIX + bot.memory['clubroom_status'] + ' '
+    topic[0] = STATUS_PREFIX + \
+        bot.memory['clubroom_status'][channel] + ' '
     topic = f'{TOPIC_SEPARATOR}'.join(topic)
-    set_topic(bot, topic)
+    set_topic(bot, channel, topic)
 
-def set_topic(bot, topic):
+def set_topic(bot, channel, topic):
     '''Set the clubroom channel's topic to given argument'''
-    bot.write(('TOPIC', CHANNEL), topic)
+    bot.write(('TOPIC', channel), topic)
